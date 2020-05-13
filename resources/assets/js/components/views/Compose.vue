d4b228b791b8440100db236be41c5a2c23202c9e
Eisson A <eisson1662@protonmail.com>
2020-05-13 02:12:38 -0500

    Create twitter project based on Vue.js.

---
+++ resources/assets/js/components/views/Compose.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <composepanel></composepanel>
+    <pending></pending>
+  </div>
+</template>
+<script>
+export default {
+  ready(){
+    this.$dispatch("saveUserDetails");
+  }
+}
+</script>
